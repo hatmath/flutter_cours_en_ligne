884acacc289957afc290b345631607558dbad385
Mathieu H <mathieuhatin@hotmail.com>
2023-09-09 23:30:34 -0400

    Commit 23:30

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_project_template_a/main.dart';
+import 'package:flutter_cours_en_ligne_v2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
