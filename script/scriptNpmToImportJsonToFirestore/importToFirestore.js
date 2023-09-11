d77a5a22e47cc522d477c0cfde9e825e8fef84c3
Mathieu H <mathieuhatin@hotmail.com>
2023-09-10 22:51:49 -0400

    script d'import json dans firebase store

---
+++ script/scriptNpmToImportJsonToFirestore/importToFirestore.js
@@ -0,0 +1,24 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json'); // Remplacez par le chemin vers votre fichier .json
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const data = require('./courses.json'); // Remplacez par le chemin vers votre fichier JSON
+
+const importData = async () => {
+  try {
+    for (const [key, value] of Object.entries(data)) {
+      const docRef = db.collection('courses').doc(key);
+      await docRef.set(value);
+    }
+    console.log('Data imported successfully');
+  } catch (error) {
+    console.error('Error importing data', error);
+  }
+}
+
+importData();
